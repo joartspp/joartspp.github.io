5464edacd8fa922c1564f8ec39cbf55786636f9b
xJoez <61728467+joartspp@users.noreply.github.com>
2021-06-07 23:11:15 +0700

    delete it allllll

--- next.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    images : {
-        domains:[
-            "http://127.0.0.1/",
-            "https://api.github.com",
-            "https://www.github.com",
-        ]
-    }
-}
--- pages/_app.js
+++
@@ -1,8 +0,0 @@
-import 'bootstrap/dist/css/bootstrap.css';
-import '../styles/globals.css';
-
-function xJoezResume({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
-
-export default xJoezResume
